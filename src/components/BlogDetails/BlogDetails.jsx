cf1d8f929d86a1bd9b6dfe89a28c801e8d793fba
progmamun <mamun333089@gmail.com>
2022-04-03 16:17:10 +0600

    blogDetails-heroiconAdded

--- src/components/BlogDetails/BlogDetails.jsx
+++ src/components/BlogDetails/BlogDetails.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { BlogContext } from '../../App';
+import { RewindIcon } from '@heroicons/react/outline';
 
 const BlogDetails = () => {
-  return <div></div>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [blogs] = useContext(BlogContext);
+
+  const blog = blogs.find(blog => blog._id === id);
+  return (
+    <>
+      <div className="header-gradient">
+        <div>
+          <button className="black-button" onClick={() => navigate(-1)}>
+            <RewindIcon></RewindIcon> <p>Back</p>
+          </button>
+        </div>
+        <div className="blog-details">
+          <div className="blog-image">
+            <img src={blog?.imageURL} alt="" />
+          </div>
+          <h1>{blog?.title}</h1>
+          <p>{blog?.blog}</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BlogDetails;
