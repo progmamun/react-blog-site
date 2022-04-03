358d1e7afeb2c521bab428f9ffc9da960520ecf9
progmamun <mamun333089@gmail.com>
2022-04-03 16:07:05 +0600

    route-setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { createContext, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Videos from './components/Videos/Videos';
+import Login from './components/Login/Login';
+import BlogDetails from './components/BlogDetails/BlogDetails';
 
 export const BlogContext = createContext();
 
 function App() {
   const [blogs, setBlogs] = useState([]);
   return (
-    <BlogContext.Provider value={[blogs, setBlogs]}></BlogContext.Provider>
+    <BlogContext.Provider value={[blogs, setBlogs]}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/videos" element={<Videos></Videos>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/blog/:id" element={<BlogDetails></BlogDetails>}></Route>
+      </Routes>
+    </BlogContext.Provider>
   );
 }
 
---
+++ src/components/BlogDetails/BlogDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BlogDetails = () => {
+  return <div></div>;
+};
+
+export default BlogDetails;
