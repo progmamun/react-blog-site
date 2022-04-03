eee34a290c386ecfa4c8cf638bbe96a25338dcd2
progmamun <mamun333089@gmail.com>
2022-04-03 15:59:25 +0600

    navigation

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import Logo from '../../Assets/Image/logo.png';
+import './Navbar.css';
+
+const Navbar = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <nav
+      style={
+        pathname.includes('blog') ? { display: 'none' } : { display: 'flex' }
+      }
+    >
+      <div className="logo-container">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="link-container">
+        <NavLink
+          className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+          to="/videos"
+        >
+          Videos
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+          to="/login"
+        >
+          Login
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
