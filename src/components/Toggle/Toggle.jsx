7fd98da0bee04f75a2b12199cfd88e27ac4ecd98
progmamun <mamun333089@gmail.com>
2022-04-12 17:30:06 +0600

    toggle

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react';
+import { ThemeContext } from '../../context';
 import Sun from '../../Assets/Image/sun.png';
 import Moon from '../../Assets/Image/moon.png';
+import './Toggle.css';
 
 const Toggle = () => {
-  const theme = useContext()
+  const theme = useContext(ThemeContext);
 
-  const handleClick = ()
+  const handleClick = () => {
+    theme.dispatch({ type: 'TOGGLE' });
+  };
   return (
-    <div className='t'>
-      <img src={Sun} alt="" />
-      <img src={Moon} alt="" />
+    <div className="t">
+      <img className="t-icon" src={Sun} alt="" />
+      <img className="t-icon" src={Moon} alt="" />
       <div className="t-button" onClick={handleClick}></div>
     </div>
   );
