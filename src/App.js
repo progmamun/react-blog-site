9097169955eee1b8dca8642f41254d48eb914679
progmamun <mamun333089@gmail.com>
2022-04-12 18:01:13 +0600

    dark-mode

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import { createContext, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createContext, useContext, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Videos from './components/Videos/Videos';
 import Login from './components/Login/Login';
 import BlogDetails from './components/BlogDetails/BlogDetails';
+import Toggle from './components/Toggle/Toggle';
+import { ThemeContext } from './context';
 
 export const BlogContext = createContext();
 
 function App() {
   const [blogs, setBlogs] = useState([]);
+
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <BlogContext.Provider value={[blogs, setBlogs]}>
-      <BrowserRouter>
+    <div
+      style={{
+        backgroundColor: darkMode ? '#222' : '#fff',
+        color: darkMode && '#fff',
+      }}
+    >
+      <BlogContext.Provider value={[blogs, setBlogs]}>
         <Navbar></Navbar>
+        <Toggle />
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="/videos" element={<Videos></Videos>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
           <Route path="/blog/:id" element={<BlogDetails></BlogDetails>}></Route>
         </Routes>
-      </BrowserRouter>
-    </BlogContext.Provider>
+      </BlogContext.Provider>
+    </div>
   );
 }
 
--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -14,7 +14,11 @@ const Toggle = () => {
     <div className="t">
       <img className="t-icon" src={Sun} alt="" />
       <img className="t-icon" src={Moon} alt="" />
-      <div className="t-button" onClick={handleClick}></div>
+      <div
+        className="t-button"
+        onClick={handleClick}
+        style={{ left: theme.state.darkMode ? 0 : 25 }}
+      ></div>
     </div>
   );
 };
